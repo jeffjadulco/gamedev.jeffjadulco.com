643e3cf06328614653739cc662d14b91e4e295de
Jeff Jadulco <dev@jeffjadulco.com>
2020-09-05 21:54:25 +0800

    :lipstick: Improve next component layout

--- components/next.js
+++ components/next.js
@@ -5,9 +5,9 @@ import { Footer } from "./footer";
 export const Next = () => {
   const { nextProject: project } = useNextProject();
   return (
-    <div className="pt-24 pb-16 bg-gray-950 border-b-4 border-teal-300">
+    <div className="pt-12 md:pt-24 pb-16 bg-gray-950 border-b-4 border-teal-300">
       <div className="container m-auto h-full flex flex-col justify-between space-y-40">
-        <div className="group flex flex-col-reverse lg:flex-row justify-center items-center space-x-6">
+        <div className="group flex flex-col-reverse lg:flex-row justify-center md:items-center space-x-0 lg:space-x-6">
           <Link href={`/project/${project.id}`}>
             <a className="px-8 md:px-0 md:h-64 md:w-120 overflow-hidden">
               <img
@@ -18,9 +18,11 @@ export const Next = () => {
             </a>
           </Link>
           <Link href={`/project/${project.id}`}>
-            <a className="text-gray-600 group-hover:text-teal-300 transition-colors duration-200">
-              <div className="text-4xl font-bold">Next</div>
-              <div className="-mt-5 text-6xl font-bold">{project.title}</div>
+            <a className="px-8 md:px-0 text-gray-600 group-hover:text-teal-300 transition-colors duration-200">
+              <div className="text-2xl sm:text-4xl font-bold">Next</div>
+              <div className="-mt-3 md:-mt-5 text-4xl sm:text-6xl font-bold">
+                {project.title}
+              </div>
             </a>
           </Link>
         </div>
